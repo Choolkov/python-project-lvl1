256a938905d25bcc4592d22d2f36fe19ff6f0aac
Global <global@mail.env>
2022-04-24 16:42:50 +0300

    add gcd game

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,39 @@
+"""GCD logic."""
+
+from random import randint
+
+from brain_games.games.engine import ROUNDS_AMOUNT, game
+
+min_number = 1
+max_number = 20
+
+
+def gcd(value1: int, value2: int) -> int:
+    """
+    Return GCD of two numbers.
+
+    Args:
+        value1: firts value
+        value2: second value
+
+    Returns:
+        int
+
+    """
+    return gcd(value2, value1 % value2) if value2 else value1
+
+
+def gcd_game():
+    """GCD game."""
+    rules = 'Find the greatest common divisor of given numbers.'
+    rounds = []
+    for _ in range(ROUNDS_AMOUNT):
+        number1 = randint(1, max_number)
+        number2 = randint(1, max_number)
+        numbers = '{0} {1}'.format(number1, number2)
+        rounds.append((numbers, str(gcd(number1, number2))))
+    game(rules, rounds)
+
+
+if __name__ == '__main__':
+    gcd_game()
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""Script for brain-gcd game."""
+from brain_games.games.gcd import gcd_game
+
+
+def main():
+    """Start brain-gcd game."""
+    gcd_game()
+
+
+if __name__ == '__main__':
+    main()

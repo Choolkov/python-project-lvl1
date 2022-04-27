736fe0b77404a2f610a431ad190ac4d1affcbd94
Choolkov <s.v.chuklov@ya.ru>
2022-04-27 09:59:36 +0300

    refactored even game

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -2,15 +2,24 @@
 
 from random import randint
 
-from brain_games.engine import ROUNDS_AMOUNT, game
+
+def get_rules() -> str:
+    """
+    Return rules of the game.
+
+    Returns:
+        str
+    """
+    return 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def even_game():
-    """Even game."""
-    rules = 'Answer "yes" if the number is even, otherwise answer "no".'
-    rounds = []
-    for _ in range(ROUNDS_AMOUNT):
-        number = randint(1, 100)
-        right_answer = 'no' if number % 2 else 'yes'
-        rounds.append((str(number), right_answer))
-    game(rules, rounds)
+def get_round() -> tuple:
+    """
+    Return round for even-game.
+
+    Returns:
+        tuple: question and right answer
+    """
+    number = randint(1, 100)
+    right_answer = 'no' if number % 2 else 'yes'
+    return (str(number), right_answer)
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 """Script for brain-even game."""
-from brain_games.games.even import even_game
+from brain_games.engine import game_engine
+from brain_games.games import even
 
 
 def main():
     """Start brain-even game."""
-    even_game()
+    game_engine(even)
 
 
 if __name__ == '__main__':
